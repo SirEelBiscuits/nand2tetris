d45b74ccfabed6768685be0165a4d5b3387d0c58
Jameson Thatcher <jameson@bluescreenofdoom.com>
2016-08-18 22:25:04 +0100

    Fill program done

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,38 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(LOOP)
+	@24576
+	D=M
+	@BLACK
+	D;JNE
+(WHITE)
+	@0
+	M=0
+	@DRAW
+	A;JMP
+(BLACK)
+	@0
+	M=-1
+(DRAW)
+	@16384
+	D=A
+	@1
+	M=D    //R1 has pixel address
+(DRAWLOOP)
+	@0
+	D=M    //D has pixel colour
+	@1     //M has pixel address
+	A=M
+	M=D    //pixel set
+	D=A+1
+	@1
+	M=D
+	@24576
+	D=D-A
+	@DRAWLOOP
+	D;JLT
+	@LOOP
+	A;JMP
+	
