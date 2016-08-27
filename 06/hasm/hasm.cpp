3bffde986de78ad48864b7c3553113cc7738b526
Jameson Thatcher <sireel@bluescreenofdoom.com>
2016-08-27 11:23:06 +0100

    file io working

--- 06/hasm/hasm.cpp
+++ 06/hasm/hasm.cpp
@@ -14,14 +14,16 @@
 #include "types.h"
 #include "parser.h"
 
-InstructionList getWholeFile();
+InstructionList getWholeFile(string FileName);
 SymbolTable     getSymbols  (InstructionList lines);
-int             writeFile   (string fileContents);
+int             writeFile   (string fileContents, string outFileName);
 
 int main(int argc, char* argv[]) {
-	auto wholeFile = getWholeFile();
+	if(argc < 3)
+		return EXIT_FAILURE;
+	auto wholeFile = getWholeFile(argv[1]);
 	auto output    = assembleFile(wholeFile);
-	return writeFile(output);
+	return writeFile(output, argv[2]);
 }
 
 InstructionList getWholeFile(string fileName) {
@@ -31,12 +33,16 @@ InstructionList getWholeFile(string fileName) {
 	file.open(fileName);
 	string line;
 	InstructionList lines{};
-	while(getline(file, line)
+	while(getline(file, line))
 			lines.push_back(line);
 
 	return lines;
 }
 
-int writeFile(string fileContents) {
+int writeFile(string fileContents, string outFileName) {
+	using std::ofstream;
+	ofstream file;
+	file.open(outFileName);
+	file << fileContents;
 	return EXIT_SUCCESS;
 }
